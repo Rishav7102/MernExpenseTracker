a3c45d30bb824441fa171ee64ff9e3c79fad5f26
Rishav7102 <101083307+Rishav7102@users.noreply.github.com>
2024-06-03 20:07:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  app: {
-    proxy: {
-      '/api': 'https://mernexpensetracker.onrender.com',
-    },
-  },
+  // app: {
+  //   proxy: {
+  //     '/api': 'https://mernexpensetracker.onrender.com',
+  //   },
+  // },
   plugins: [react()],
 })
